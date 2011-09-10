bf65ef91c73062cf999edacbba655b549d95f0c8
nerdopolis <nerdopolis@3df0df1f-9518-46d5-b0fc-6e62cabb456f>
2011-09-10 13:19:30 +0000

    /usr/LinuxRCD-Recovery-Tools-And-Data is being changed to /LinuxRCD-Recovery-Tools-And-Data as many build scripts do mkdir usr instead of mkdir -p usr.
    change the debhelper scripts to point to LinuxRCD-Recovery-Tools-And-Data
    
    Need to fix Debian Quilt issues where it tries to use a tar...
    
    git-svn-id: svn://svn.code.sf.net/p/linuxrcd/code@44 3df0df1f-9518-46d5-b0fc-6e62cabb456f

--- linuxrcd_package_files/chrootscript.sh
+++ linuxrcd_package_files/chrootscript.sh
@@ -18,37 +18,36 @@
 
 #mount the procfs
 mount -t proc none /proc
-
-
 #mount sysfs
 mount -t sysfs none /sys
-
-
-
 #mount /dev/pts
 mount -t devpts none /dev/pts
+
+
 
 
 #update the apt cache
 apt-get update
 
-#install aptitude
-echo Y | apt-get install aptitude 
+
+
+#install aptitude and locales and a language pack 
+echo Y | apt-get install aptitude locales language-pack-en 
 
 #Capture the list of all the default installed packages
 dpkg --get-selections > /usr/packages_in_minimal
 
-#install the CD's packages here
+#install the CD's packages here TODO
+
 
 #capture the list of all installed packages. They are the ones that will be built.
 dpkg --get-selections | grep -v deinstall | awk '{print $1}' > /usr/packages_to_build
 
-#Put the contents of /usr into /usr/LinuxRCD-Recovery-Tools-And-Data as packages that are being built will have ALL references to /usr changed to /usr/LinuxRCD-Recovery-Tools-And-Data
-ln -s /usr /usr/LinuxRCD-Recovery-Tools-And-Data
-
-
 #Capture the list of all the default installed packages
 dpkg --set-selections < /usr/packages_in_minimal
+
+#revert to minimal install
+echo y | apt-get -u dselect-upgrade
 
 #install utilites that will help the rebuild of packages
 aptitude install binutils devscripts bzr build-essential fakeroot debian-builder  -y --without-recommends
@@ -56,21 +55,91 @@ aptitude install binutils devscripts bzr build-essential fakeroot debian-builder
 #Capture the list of all the installed packages for building
 dpkg --get-selections > /usr/packages_for_building
 
+
+#make /usr and /usr/local the same thing
+cp -R /usr/local/* /usr/
+mount --bind /usr/ /usr/local
+
+#Put the contents of /usr into /LinuxRCD-Recovery-Tools-And-Data as packages that are being built will have ALL references to /usr changed to /LinuxRCD-Recovery-Tools-And-Data and will be looking for the files here
+mount --rbind /usr /LinuxRCD-Recovery-Tools-And-Data
+
+
+
+
+
+#get the number of packages to build
+numberofpackages=$(cat /usr/packages_to_build | grep -c ^)
+
+#change into the /usr/share/debhelper folder
+cd /usr/share/debhelper
+
+#change into the usr folder
+#Change all references to /usr to /LinuxRCD-Recovery-Tools-And-Data
+find . -name "*" |while read FILE
+do
+#sed -i 's/usr/LinuxRCD-Recovery-Tools-And-Data/g' $FILE  
+sed -i 's/\/usr\//USR_FOLDER_NAME_WITH_DUAL_SLASHES/g' $FILE
+sed -i 's/\/usr/USR_FOLDER_NAME_WITH_START_SLASH/g' $FILE
+sed -i 's/usr\//USR_FOLDER_NAME_WITH_END_SLASH/g' $FILE
+sed -i 's/\<usr\>/USR_FOLDER_NAME_ISOLATED/g' $FILE
+
+sed -i 's/USR_FOLDER_NAME_WITH_DUAL_SLASHES/\/LinuxRCD-Recovery-Tools-And-Data\//g' $FILE
+sed -i 's/USR_FOLDER_NAME_WITH_START_SLASH/\/LinuxRCD-Recovery-Tools-And-Data/g' $FILE
+sed -i 's/USR_FOLDER_NAME_WITH_END_SLASH/LinuxRCD-Recovery-Tools-And-Data\//g' $FILE
+sed -i 's/USR_FOLDER_NAME_ISOLATED/LinuxRCD-Recovery-Tools-And-Data/g' $FILE
+done
+
+find . -name usr | while read FILEPATH
+do
+#FILEPATH=$(readlink -f $FILEPATH) 
+oldfilepath=$(echo $FILEPATH | rev | cut -f2- -d '/' | rev)
+newfilepath=$(echo $oldfilepath |sed 's/usr/LinuxRCD-Recovery-Tools-And-Data/g')
+filename=$(echo $FILEPATH | rev | awk -F / '{print $1}' | rev)
+mkdir -p $newfilepath/LinuxRCD-Recovery-Tools-And-Data
+cp -R $oldfilepath/usr/* $newfilepath/LinuxRCD-Recovery-Tools-And-Data
+done
+
 #change into the packagebuild folder
 cd /usr/packagebuild
 
+packagenumber=1
+failedpackages=0
+successfulpackages=0
 #build the packages using the packagebuild script.
-cat /usr/packages_to_build | while read PACKAGE
+while read PACKAGE
 do
-packagebuild $PACKAGE
-done
+echo "Building package "$PACKAGE": "$packagenumber" of "$numberofpackages""
+#build the package
+packagebuild "$PACKAGE" &> "$PACKAGE".log 
+buildstatus=$?
+
+if [ "$buildstatus" -eq 0 ]
+then
+echo "Build of "$PACKAGE" Successful"
+successfulpackages=$(( $successfulpackages+1 ))
+else
+echo "build of "$PACKAGE" Failed"
+failedpackages=$(( $failedpackages+1 ))
+fi
+
+packagenumber=$(( $packagenumber+1 ))
+done < <(cat /usr/packages_to_build)
+
+echo "$successfulpackages packages where built, $failedpackages failed to build"
+
+
 
 #Prepare the folder for being a local repo
 cd /usr/packageoutput
 dpkg-scanpackages . /dev/null | gzip -c9 > Packages.gz
 
 
+
+
 #unmount the procfs from the chrooted system
 umount proc
 
-
+#unmount the /usr/local bind mount
+umount /usr/local
+#unmount the /usr bind mount
+umount /usr/LinuxRCD-Recovery-Tools-And-Data
