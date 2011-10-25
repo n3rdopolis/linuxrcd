9a60c8019429674860743c540ecef0b2c29b2ced
nerdopolis <nerdopolis@3df0df1f-9518-46d5-b0fc-6e62cabb456f>
2011-10-25 23:42:32 +0000

    Fix the recursive rename of the folders. something like /usr/usr was failing to become /RCD/RCD.
    git-svn-id: svn://svn.code.sf.net/p/linuxrcd/code@121 3df0df1f-9518-46d5-b0fc-6e62cabb456f

--- linuxrcd_edit.sh
+++ linuxrcd_edit.sh
@@ -21,7 +21,7 @@ export OriginalText=$1
 export TargetText=$2
 
 #Change all references to /$OriginalText to /$TargetText in the folder containg the LiveCD system
-find "/media/LiveDiskCreAtionChrootFolDer" -type f   -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/usr/bin/recoverylauncher" |while read FILE
+find "/media/LiveDiskCreAtionChrootFolDer" -type f   -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/usr/bin/recoverylauncher" | sort -r | while read FILE
 do
 echo "editing file $FILE"
 #replace all instances of $OriginalText with the new folder name only if its not near a-z A-Z or 0-9. Thanks to @ofnuts on Ubuntu Fourms for helping me with the sed expression
@@ -31,7 +31,7 @@ rm "$FILE.tmp"
 done
 
 #change all symbolic links that point to $OriginalText to point to $TargetText
-find "/media/LiveDiskCreAtionChrootFolDer" -type l   -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*"  |while read FILE
+find "/media/LiveDiskCreAtionChrootFolDer" -type l   -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*" | sort -r  |while read FILE
 do
 echo "relinking $FILE"
 newlink=$(readlink $FILE | sed -re "s/(\W|^)$OriginalText(\W|$)/\1$TargetText\2/g")
@@ -39,11 +39,8 @@ ln -s -f "$newlink" "$FILE"
 done
 
 #find all items contianing $OriginalText in the name
-find "/media/LiveDiskCreAtionChrootFolDer" -name "*$OriginalText*"  -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*" | while read FILEPATH
+find "/media/LiveDiskCreAtionChrootFolDer" -name "*$OriginalText*"  -not -path "/media/LiveDiskCreAtionChrootFolDer/proc/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/sys/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/dev/*" -not -path "/media/LiveDiskCreAtionChrootFolDer/tmp/*" | sort -r | while read FILEPATH
 do
-echo "Renaming $FILEPATH"
-rename "s/(\W|^)$OriginalText(\W|$)/\1$TargetText\2/g" "$FILEPATH"
+cd "$FILEPATH"
+rename -v "s/(\W|^)$OriginalText(\W|$)/\1$TargetText\2/g" * 2> /dev/null
 done
-
-
-
