5ff2d509801a4e5ae8cc92e160c3cc5c165d5ae0
nerdopolis <nerdopolis@3df0df1f-9518-46d5-b0fc-6e62cabb456f>
2014-08-31 16:06:26 +0000

    Add more files from RebeccaBlackOS
    git-svn-id: svn://svn.code.sf.net/p/linuxrcd/code@143 3df0df1f-9518-46d5-b0fc-6e62cabb456f

---
+++ linuxrcd_files/tmp/configure_phase3.sh
@@ -0,0 +1,121 @@
+#! /bin/bash
+#    Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014 nerdopolis (or n3rdopolis) <bluescreen_avenger@verzion.net>
+#
+#    This file is part of LinuxRCD
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#function to handle moving back dpkg redirect files for chroot
+function RevertFile {
+  TargetFile=$1
+  SourceFile=$(dpkg-divert --truename "$1")
+  if [[ "$TargetFile" != "$SourceFile" ]]
+  then
+    rm "$1"
+    dpkg-divert --local --rename --remove "$1"
+  fi
+}
+
+#function to handle temporarily moving files with dpkg that attempt to cause issues with chroot
+function RedirectFile {
+  RevertFile "$1"
+  dpkg-divert --local --rename --add "$1" 
+  ln -s /bin/true "$1"
+}
+
+
+#Copy the import files into the system, and create menu items while creating a deb with checkinstall.
+cd /tmp
+mkdir debian
+touch debian/control
+#remove any old deb files for this package
+rm "/srcbuild/buildoutput/"lrcd-lrcd_*.deb
+checkinstall -y -D --nodoc --dpkgflags=--force-overwrite --install=yes --backup=no --pkgname=rbos-rbos --pkgversion=1 --pkgrelease=$(date +%s)  --maintainer=rbos@rbos --pkgsource=rbos --pkggroup=rbos --requires="kde-baseapps-bin" /tmp/configure_phase3_helper.sh
+cp *.deb "/srcbuild/buildoutput/"
+cd $OLDPWD
+
+#copy all files
+rsync /usr/import/* -a /
+
+#delete the import folder
+rm -r /usr/import
+
+#run the script that calls all compile scripts in a specified order, in build only mode
+compile_all build-only
+
+#save the build date of the CD.
+echo "$(date)" > /etc/builddate
+
+#Get all Source 
+cat /usr/share/logs/build_core/*/GetSourceVersion > /usr/share/build_core_revisions.txt
+
+#hide buildlogs in tmp from remastersys
+mv /usr/share/logs	/tmp
+
+# #start the remastersys job
+# remastersys dist
+# 
+# mv /home/remastersys/remastersys/custom.iso /home/remastersys/remastersys/custom-full.iso
+# 
+# 
+# 
+# #Redirect these utilitues to /bin/true during the live CD Build process. They aren't needed and cause package installs to complain
+# RedirectFile /usr/sbin/grub-probe
+# RedirectFile /sbin/initctl
+# RedirectFile /usr/sbin/invoke-rc.d
+# 
+# #This will remove my abilities to build packages from the ISO, but should make it a bit smaller
+# REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dev$"  | grep -v python-dbus-dev | grep -v dpkg-dev)
+# 
+# apt-get purge $REMOVEDEVPGKS -y --force-yes | tee /tmp/logs/package_operations/removes.txt
+# 
+# 
+# REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dev:"  | grep -v python-dbus-dev | grep -v dpkg-dev)
+# apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
+# 
+# 
+# REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dbg$"  | grep -v python-dbus-dev | grep -v dpkg-dev)
+# apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
+# 
+# REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dbg:"  | grep -v python-dbus-dev | grep -v dpkg-dev)
+# apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
+# 
+# REMOVEDEVPGKS="texlive-base ubuntu-docs gnome-user-guide cmake libgl1-mesa-dri-dbg libglib2.0-doc valgrind cmake-rbos smbclient freepats libc6-dbg doxygen git subversion bzr mercurial checkinstall texinfo"
+# apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
+# 
+# 
+# apt-get autoremove -y --force-yes >> /tmp/logs/package_operations/removes.txt
+# 
+# #Reset the utilites back to the way they are supposed to be.
+# RevertFile /usr/sbin/grub-probe
+# RevertFile /sbin/initctl
+# RevertFile /usr/sbin/invoke-rc.d
+
+# #Reduce binary sizes
+# echo "Reducing binary file sizes"
+# find /opt/bin /opt/lib /opt/sbin | while read FILE
+# do
+#   strip $FILE 2>/dev/null
+# done
+
+#clean more apt stuff
+apt-get clean
+rm -rf /var/cache/apt-xapian-index/*
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/lib/dlocate/*
+#start the remastersys job
+remastersys dist
+
+#move logs back
+mv /tmp/logs /usr/share
---
+++ linuxrcd_files/tmp/configure_phase3_helper.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+#    Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014 nerdopolis (or n3rdopolis) <bluescreen_avenger@verzion.net>
+#
+#    This file is part of LinuxRCD
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#This file is used by checkinstall for creating the rbos-rbos package that has all of the installed SVN files
+
+#Copy select files into place, that are suitable for distribution.
+mkdir -p /usr/bin
+cp -a /usr/import/usr/bin/* /usr/bin
+
+mkdir -p /usr/libexec
+cp -a /usr/import/usr/libexec/* /usr/libexec
+
+mkdir -p /usr/share/LRCD_MENU
+cp -a /usr/import/usr/share/LRCD_MENU/* /usr/share/LRCD_MENU
+
+mkdir -p /usr/share/LRCD_PATCHES
+cp -a /usr/import/usr/share/LRCD_PATCHES/* /usr/share/LRCD_PATCHES
+
+mkdir -p /usr/share/icons
+cp -a /usr/import/usr/share/icons/* /usr/share/icons
+
+mkdir -p /usr/share/xsessions
+cp -a /usr/import/usr/share/xsessions/* /usr/share/xsessions
+
+mkdir -p /etc/skel/.config
+cp -a /usr/import/etc/skel/* /etc/skel
+
+mkdir -p /root
+cp -a /usr/import/root/* /etc/skel/root
+
+mkdir -p /etc/lightdm
+cp -a /usr/import/etc/lightdm/* /etc/lightdm
+
+mkdir -p /etc/pam.d
+cp -a /usr/import/etc/pam.d/* /etc/pam.d
+
+mkdir -p /etc/sysctl.d
+cp -a /usr/import/etc/sysctl.d/* /etc/sysctl.d
+
+mkdir -p /lib
+cp -a /usr/import/lib/* /lib
+
+mkdir -p /etc/systemd
+cp -a /usr/import/etc/systemd/* /etc/systemd
+
+mkdir -p /etc/init
+cp -a /usr/import/etc/init/* /etc/init
+
+mkdir -p /usr/lib/tmpfiles.d
+cp -a /usr/import/usr/lib/tmpfiles.d/* /usr/lib/tmpfiles.d
+
+mkdir -p /etc/X11
+cp -a /usr/import/etc/X11/* /etc/X11
+
+mkdir -p /etc/loginmanagerdisplay
+cp -a /usr/import/etc/loginmanagerdisplay/* /etc/loginmanagerdisplay
+
+#install the menu items for the wayland tests
+install_menu_items
+
+#Set the cursor theme
+update-alternatives --set x-cursor-theme /etc/X11/cursors/oxy-white.theme
