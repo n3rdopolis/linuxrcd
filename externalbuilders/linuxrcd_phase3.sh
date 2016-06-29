c3ffd28faa503af59f195315848b0fa175efa7b6
nerdopolis <nerdopolis@3df0df1f-9518-46d5-b0fc-6e62cabb456f>
2016-06-29 21:51:34 +0000

    Move Phase3 external builder to the lastest from RBOS
    git-svn-id: svn://svn.code.sf.net/p/linuxrcd/code@216 3df0df1f-9518-46d5-b0fc-6e62cabb456f

--- externalbuilders/linuxrcd_phase3.sh
+++ externalbuilders/linuxrcd_phase3.sh
@@ -22,7 +22,7 @@ function linuxrcdedit ()
   export TargetText=$2
 
   #Change all references to /$OriginalText to /$TargetText in the folder containg the LiveCD system
-  find ""$BUILDLOCATION"/build/$BUILDARCH/workdir" -type f   -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/bin/recoverylauncher" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/RCDbin/recoverychrootscript" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/launchers/Apps" | sort -r | while read FILE
+  find ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir" -type f   -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/tmp/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/bin/recoverylauncher" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/RCDbin/recoverychrootscript" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/launchers/Apps" | sort -r | while read FILE
   do
     echo "editing file $FILE"
     #replace all instances of $OriginalText with the new folder name only if its not near a-z A-Z or 0-9. Thanks to @ofnuts on Ubuntu Fourms for helping me with the sed expression
@@ -32,7 +32,7 @@ function linuxrcdedit ()
   done
 
   #change all symbolic links that point to $OriginalText to point to $TargetText
-  find ""$BUILDLOCATION"/build/$BUILDARCH/workdir" -type l   -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/*" | sort -r  |while read FILE
+  find ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir" -type l   -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/tmp/*" | sort -r  |while read FILE
   do
     echo "relinking $FILE"
     newlink=$(readlink $FILE | sed -re "s/(\W|^)$OriginalText(\W|$)/\1$TargetText\2/g")
@@ -40,7 +40,7 @@ function linuxrcdedit ()
   done
 
   #find all items contianing $OriginalText in the name
-  find ""$BUILDLOCATION"/build/$BUILDARCH/workdir"  -type d  -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/*" | sort -r | while read FILEPATH
+  find ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir"  -type d  -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/proc/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/sys/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/dev/*" -not -path ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/tmp/*" | sort -r | while read FILEPATH
   do
     cd "$FILEPATH"
     rename -v "s/(\W|^)$OriginalText(\W|$)/\1$TargetText\2/g" * 2> /dev/null
@@ -53,7 +53,7 @@ function RenameFiles()
   linuxrcdedit lib LYB
   linuxrcdedit lib64 LYB64
   #fix for Xorg, it uses wildcards.
-  find "$BUILDLOCATION"/build/$BUILDARCH/workdir/RCD/LYB/xorg -name "lib*"   | while read FILEPATH
+  find "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/RCD/LYB/xorg -name "lib*"   | while read FILEPATH
   do
     echo "Renaming $FILEPATH"
     rename "s/lib/\1LYB\2/g" "$FILEPATH"
@@ -61,24 +61,24 @@ function RenameFiles()
 
 
   #fix for NetworkManager, it uses wildcards.
-  find "$BUILDLOCATION"/build/$BUILDARCH/workdir/RCD/LYB/NetworkManager -name "lib*"   | while read FILEPATH
+  find "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/RCD/LYB/NetworkManager -name "lib*"   | while read FILEPATH
   do
     echo "Renaming $FILEPATH"
     rename "s/lib/\1LYB\2/g" "$FILEPATH"
   done
 
   #Do this for X
-  ln -s -f /var/LYB "$BUILDLOCATION"/build/$BUILDARCH/workdir/var/lib
+  ln -s -f /var/LYB "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/var/lib
 
   #delete the usr folder in the Live CD
-  rm -rf "$BUILDLOCATION"/build/$BUILDARCH/workdir/usr
+  rm -rf "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr
 
   #Do this for OS prober as it works with a normal system with lib. not LYB
-  sed -i 's/LYB/lib/g' "$BUILDLOCATION"/build/$BUILDARCH/workdir/RCD/LYB/os-probes/mounted/90linux-distro
+  sed -i 's/LYB/lib/g' "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/RCD/LYB/os-probes/mounted/90linux-distro
 
   #Do this for the main library interpreter, so that it does not use the target system's ld.so.cache
-  sed -i 's@/ld.so.cache@/LD.SO.CACHE@g' "$BUILDLOCATION"/build/$BUILDARCH/workdir/LYB/ld-linux*
-  mv "$BUILDLOCATION"/etc/ld.so.cache "$BUILDLOCATION"/build/$BUILDARCH/workdir/etc/LD.SO.CACHE
+  sed -i 's@/ld.so.cache@/LD.SO.CACHE@g' "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/LYB/ld-linux*
+  mv "$BUILDLOCATION"/etc/ld.so.cache "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/etc/LD.SO.CACHE
 
 
 }
@@ -87,59 +87,82 @@ echo "PHASE 3"
 SCRIPTFILEPATH=$(readlink -f "$0")
 SCRIPTFOLDERPATH=$(dirname "$SCRIPTFILEPATH")
 
+HASOVERLAYFS=$(grep -c overlay$ /proc/filesystems)
+if [[ $HASOVERLAYFS == 0 ]]
+then
+  HASOVERLAYFSMODULE=$(modprobe -n overlay; echo $?)
+  if [[ $HASOVERLAYFSMODULE == 0 ]]
+  then
+    HASOVERLAYFS=1
+  fi
+fi
+
 HOMELOCATION=~
 unset HOME
 
-if [[ -z $BUILDARCH || -z $BUILDLOCATION || $UID != 0 ]]
+if [[ -z "$BUILDARCH" || -z $BUILDLOCATION || $UID != 0 ]]
 then
   echo "BUILDARCH variable not set, or BUILDLOCATION not set, or not run as root. This external build script should be called by the main build script."
   exit
 fi
 
 #create a folder for the media mountpoints in the media folder
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/phase_1
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/phase_2
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/phase_3
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/srcbuild/buildoutput
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/buildoutput
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/workdir
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/archives
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/remastersys
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/vartmp
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/phase_1
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/phase_2
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/phase_3
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/srcbuild/buildoutput
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/buildoutput
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/archives
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/remastersys
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/vartmp
+
+#Ensure that all the mountpoints in the namespace are private, and won't be shared to the main system
+mount --make-rprivate /
 
 #Clean up Phase 3 data.
-rm -rf "$BUILDLOCATION"/build/$BUILDARCH/phase_3/*
+rm -rf "$BUILDLOCATION"/build/"$BUILDARCH"/phase_3/*
 
-#Copy phase3 from phase2, and bind mount phase3 at the workdir
-echo "Duplicating Phase 2 for usage in Phase 3. This may take some time..."
-cp --reflink=auto -a "$BUILDLOCATION"/build/$BUILDARCH/phase_2/. "$BUILDLOCATION"/build/$BUILDARCH/phase_3
-mount --rbind "$BUILDLOCATION"/build/$BUILDARCH/phase_3 "$BUILDLOCATION"/build/$BUILDARCH/workdir
+if [[ $HASOVERLAYFS == 0 ]]
+then
+  #Copy phase3 from phase2, and bind mount phase3 at the workdir
+  echo "Duplicating Phase 2 for usage in Phase 3. This may take some time..."
+  cp --reflink=auto -a "$BUILDLOCATION"/build/"$BUILDARCH"/phase_2/. "$BUILDLOCATION"/build/"$BUILDARCH"/phase_3
+  mount --bind "$BUILDLOCATION"/build/"$BUILDARCH"/phase_3 "$BUILDLOCATION"/build/"$BUILDARCH"/workdir
+else
+  #Union mount phase2 and phase3
+  mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/unionwork
+  mount -t overlay overlay -o lowerdir="$BUILDLOCATION"/build/"$BUILDARCH"/phase_2,upperdir="$BUILDLOCATION"/build/"$BUILDARCH"/phase_3,workdir="$BUILDLOCATION"/build/"$BUILDARCH"/unionwork "$BUILDLOCATION"/build/"$BUILDARCH"/workdir
+fi
+
 
 #mounting critical fses on chrooted fs with bind 
-mount --rbind /dev "$BUILDLOCATION"/build/$BUILDARCH/workdir/dev/
-mount --rbind /proc "$BUILDLOCATION"/build/$BUILDARCH/workdir/proc/
-mount --rbind /sys "$BUILDLOCATION"/build/$BUILDARCH/workdir/sys/
+mount --rbind /dev "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/dev
+mount --rbind /proc "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/proc
+mount --rbind /sys "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/sys
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/run/shm
+mount --bind /run/shm "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/run/shm
 
 #Mount in the folder with previously built debs
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/workdir/srcbuild/buildoutput
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/workdir/home/remastersys
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/workdir/var/tmp
-mount --rbind "$BUILDLOCATION"/build/$BUILDARCH/srcbuild "$BUILDLOCATION"/build/$BUILDARCH/workdir/srcbuild
-mount --rbind "$BUILDLOCATION"/build/$BUILDARCH/buildoutput "$BUILDLOCATION"/build/$BUILDARCH/workdir/srcbuild/buildoutput
-mount --rbind  "$BUILDLOCATION"/build/$BUILDARCH/remastersys "$BUILDLOCATION"/build/$BUILDARCH/workdir/home/remastersys
-mount --rbind  "$BUILDLOCATION"/build/$BUILDARCH/vartmp "$BUILDLOCATION"/build/$BUILDARCH/workdir/var/tmp
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/srcbuild/buildoutput
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/home/remastersys
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/var/tmp
+mount --bind "$BUILDLOCATION"/build/"$BUILDARCH"/srcbuild "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/srcbuild
+mount --bind "$BUILDLOCATION"/build/"$BUILDARCH"/buildoutput "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/srcbuild/buildoutput
+mount --bind  "$BUILDLOCATION"/build/"$BUILDARCH"/remastersys "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/home/remastersys
+mount --bind  "$BUILDLOCATION"/build/"$BUILDARCH"/vartmp "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/var/tmp
 
 #copy the files to where they belong
-rsync "$BUILDLOCATION"/build/$BUILDARCH/importdata/* -Cr "$BUILDLOCATION"/build/$BUILDARCH/workdir/
+rsync "$BUILDLOCATION"/build/"$BUILDARCH"/importdata/* -Cr "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/
 
 #Handle /usr/import for the creation of the deb file that contains this systems files
-mkdir -p "$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/import
-rsync "$BUILDLOCATION"/build/$BUILDARCH/importdata/* -Cr "$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/import
-rm -rf "$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/import/usr/import
+mkdir -p "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/import
+rsync "$BUILDLOCATION"/build/"$BUILDARCH"/importdata/* -Cr "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/import
+rm -rf "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/import/usr/import
 
 #delete the temp folder
-rm -rf "$BUILDLOCATION"/build/$BUILDARCH/workdir/temp/
+rm -rf "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/temp/
 
 
 #Configure the Live system########################################
@@ -159,20 +182,27 @@ fi
 ENDDATE=$(date +"%Y-%m-%d %H-%M-%S")
 
 #Create a folder for the log files with the date string
-mkdir -p ""$BUILDLOCATION"/logs/$ENDDATE $BUILDARCH"
+mkdir -p ""$BUILDLOCATION"/logs/$ENDDATE "$BUILDARCH""
 
 #Export the log files to the location
-cp -a ""$BUILDLOCATION"/build/$BUILDARCH/phase_1/usr/share/logs/"* ""$BUILDLOCATION"/logs/$ENDDATE $BUILDARCH"
-cp -a ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/share/logs/"* ""$BUILDLOCATION"/logs/$ENDDATE $BUILDARCH"
-rm ""$BUILDLOCATION"/logs/latest"
-ln -s ""$BUILDLOCATION"/logs/$ENDDATE $BUILDARCH" ""$BUILDLOCATION"/logs/latest"
-cp -a ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/share/build_core_revisions.txt" ""$BUILDLOCATION"/logs/$ENDDATE $BUILDARCH" 
-cp -a ""$BUILDLOCATION"/build/$BUILDARCH/workdir/usr/share/build_core_revisions.txt" ""$HOMELOCATION"/LinuxRCD_Revisions_$BUILDARCH.txt" 
-if [[ ! -f "$BUILDLOCATION"/build/$BUILDARCH/workdir/home/remastersys/remastersys/custom.iso ]]
+cp -a ""$BUILDLOCATION"/build/"$BUILDARCH"/phase_1/usr/share/logs/"* ""$BUILDLOCATION"/logs/$ENDDATE "$BUILDARCH""
+cp -a ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/share/logs/"* ""$BUILDLOCATION"/logs/$ENDDATE "$BUILDARCH""
+rm ""$BUILDLOCATION"/logs/latest-"$BUILDARCH""
+ln -s ""$BUILDLOCATION"/logs/$ENDDATE "$BUILDARCH"" ""$BUILDLOCATION"/logs/latest-"$BUILDARCH""
+cp -a ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/share/build_core_revisions.txt" ""$BUILDLOCATION"/logs/$ENDDATE "$BUILDARCH"" 
+cp -a ""$BUILDLOCATION"/build/"$BUILDARCH"/workdir/usr/share/build_core_revisions.txt" ""$HOMELOCATION"/LinuxRCD_Revisions_"$BUILDARCH".txt"
+
+#Take a snapshot of the source
+rm "$HOMELOCATION"/LinuxRCD_Source_"$BUILDARCH".tar.gz
+tar -czvf "$HOMELOCATION"/LinuxRCD_Source_"$BUILDARCH".tar.gz -C "$BUILDLOCATION"/build/"$BUILDARCH"/exportsource/ . &>/dev/null
+
+
+#If the live cd did not build then tell user 
+if [[ ! -f "$BUILDLOCATION"/build/"$BUILDARCH"/workdir/home/remastersys/remastersys/custom.iso ]]
 then  
   ISOFAILED=1
 else
-    mv "$BUILDLOCATION"/build/$BUILDARCH/remastersys/remastersys/custom.iso "$HOMELOCATION"/LinuxRCD_$BUILDARCH.iso
+    mv "$BUILDLOCATION"/build/"$BUILDARCH"/remastersys/remastersys/custom.iso "$HOMELOCATION"/LinuxRCD_"$BUILDARCH".iso
 fi 
 
 
